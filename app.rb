3415bcd1c35390c37dcf85243200813637e3d076
chaseWilliams <bornofawesomeness@gmail.com>
2016-04-10 15:41:46 -0400

    added support for unfiltered weather data

--- app.rb
+++ app.rb
@@ -17,13 +17,24 @@ class App < Sinatra::Application
       {status: 'fail', reason: err.to_s}.to_json
     else
       weather = JSON.parse RestClient.get ENV['OWM_URI'] + "mode=coordinates&lat=#{lat}&lon=#{lon}"
-      puts weather
       wiki_article = JSON.parse RestClient.get ENV['WIKI_URI'] + "lat=#{lat}&lon=#{lon}"
       puts wiki_article
       flickr_url = JSON.parse RestClient.get ENV['FLICKR_URI'] + "temp=#{weather['data']['temp']}" +
                                                               "&id=#{weather['data']['condition_id']}"
       puts flickr_url
       {status: 'ok', weather: weather, article: wiki_article, pic: flickr_url}.to_json
+    end
+  end
+
+  get '/pure' do
+    begin
+      lat = params[:lat]
+      lon = params[:lon]
+    rescue StandardError => err
+      status 400
+      {status: 'fail', reason: err.to_s}.to_json
+    else
+      RestClient.get ENV['OWM_URI_PURE'] + "mode=coordinates&lat=#{lat}&lon=#{lon}"
     end
   end
 end

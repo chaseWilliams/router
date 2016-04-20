68b4290b9fc80f4b2a04ee4ea0ed0ec255a0b9e1
Chase Williams <bornofawesomeness@gmail.com>
2016-04-20 00:37:34 +0000

    fixed broken wiki link; now correctly redirects to wikipedia article

---
+++ router/app.rb
@@ -0,0 +1,46 @@
+require './router/loader'
+
+class App < Sinatra::Application
+
+  before do
+    content_type 'application/json'
+  end
+
+  get '/' do
+    puts ENV['OWM_URI']
+    Logger.new(STDOUT).warn 'hello'
+    begin
+      lat = params[:lat]
+      lon = params[:lon]
+    rescue StandardError => err
+      status 400
+      {status: 'fail', reason: err.to_s}.to_json
+    else
+      weather = JSON.parse RestClient.get 'http://localhost:3002/?' + "mode=coordinates&lat=#{lat}&lon=#{lon}"
+      wiki_article = JSON.parse RestClient.get 'http://localhost:3003/?' + "lat=#{lat}&lon=#{lon}"
+      puts wiki_article
+      flickr_url = JSON.parse RestClient.get 'http://localhost:3001/?' + "temp=#{weather['data']['temp']}" +
+                                                              "&id=#{weather['data']['condition_id']}"
+      puts flickr_url
+      {status: 'ok', weather: weather, article: wiki_article, pic: flickr_url}.to_json
+    end
+  end
+
+  get '/pure' do
+    begin
+      lat = params[:lat]
+      lon = params[:lon]
+    rescue StandardError => err
+      status 400
+      {status: 'fail', reason: err.to_s}.to_json
+    else
+      RestClient.get ENV['OWM_URI_PURE'] + "mode=coordinates&lat=#{lat}&lon=#{lon}"
+    end
+  end
+
+  # this is for the front end
+  set :public_folder, 'public'
+  get '/example' do
+    redirect 'index.html'
+  end
+end
